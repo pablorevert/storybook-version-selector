2e1a72809d8affdc5b8df9a71516f54fa732a176
Pablo A. Revert <pablo.revert@laincosoftware.com>
2024-08-29 18:56:03 -0300

    Borrando cosas que no se usan

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
-export const ADDON_ID = "my-addon";
+export const ADDON_ID = "version-selector";
 export const TOOL_ID = `${ADDON_ID}/tool`;
-export const PANEL_ID = `${ADDON_ID}/panel`;
-export const TAB_ID = `${ADDON_ID}/tab`;
-export const KEY = `my-addon`;
+export const KEY = `version-selector`;
 
 export const EVENTS = {
   RESULT: `${ADDON_ID}/result`,
