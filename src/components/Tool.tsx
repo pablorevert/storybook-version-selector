00bda0ce7311d1584259e4a594dcdbe04626f048
Pablo A. Revert <pablo.revert@laincosoftware.com>
2024-08-29 19:00:50 -0300

    Sigo borrando

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -5,25 +5,21 @@ import {
   TooltipLinkList,
   WithTooltip,
 } from "storybook/internal/components";
-import { ADDON_ID, KEY, TOOL_ID } from "../constants";
+import { KEY, TOOL_ID } from "../constants";
 import { CategoryIcon } from "@storybook/icons";
-import { action } from "@storybook/addon-actions";
 import { styled } from "storybook/internal/theming";
-import { on } from "events";
 
 const onClick = (_: any, v: any) => {
   const newPath = (window.location.pathname + "v").replace(
     /\/[^/]+$/,
     `/${v.path}`,
   );
-  console.log(
-    "Navigating to:",
-    window.location.origin + newPath + "/" + window.location.search,
-  );
+  window.location.href =
+    window.location.origin + newPath + window.location.search;
 };
 
 export const Tool = memo(function MyAddonSelector({ api }: { api: API }) {
-  const [globals, updateGlobals, storyGlobals] = useGlobals();
+  const [, , storyGlobals] = useGlobals();
   const [versions, setVersions] = useState([]);
   const path = "0.6.0";
   const title = versions.find((v) => v.path === path)?.title;
@@ -57,31 +53,6 @@ export const Tool = memo(function MyAddonSelector({ api }: { api: API }) {
   }, []);
 
   const isLocked = KEY in storyGlobals;
-  const isActive = !!globals[KEY];
-
-  const toggle = useCallback(() => {
-    updateGlobals({
-      [KEY]: !isActive,
-    });
-  }, [isActive]);
-
-  useEffect(() => {
-    api.setAddonShortcut(ADDON_ID, {
-      label: "Toggle Measure [O]",
-      defaultShortcut: ["O"],
-      actionName: "outline",
-      showInMenu: false,
-      action: toggle,
-    });
-  }, [toggle, api]);
-
-  const handleChange = (event) => {
-    console.log({ event });
-    const selectedVersion = event.target.value;
-    window.location.href = `/${selectedVersion}/index.html`;
-  };
-
-  console.log({ location: window.location });
 
   return (
     <div style={{ padding: "10px" }}>
@@ -93,7 +64,7 @@ export const Tool = memo(function MyAddonSelector({ api }: { api: API }) {
       >
         <IconButton
           key={TOOL_ID}
-          active={isActive}
+          active={false}
           disabled={isLocked}
           title="Enable my addon"
         >
